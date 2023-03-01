4d7b2a6dcbe45913552bdd036628290ad314a120
adarshgupta2611 <adarshgupta261199@gmail.com>
2023-03-01 12:42:01 +0530

    Flexing the Attendance Graph and Adding two paragraphs

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
-import Header from "../Admin/Header"
 import AttendanceGraph from "../AttendanceGraph";
 import styles from "./Index.module.css"
+import Header from "./Header";
 import Sidebar from "../Sidebar/Sidebar";
 
 const Index = ()=>{
@@ -12,6 +12,8 @@ const Index = ()=>{
       <div className={styles.content}>
         <Sidebar></Sidebar>
         <div className={styles.graphDiv}>
+          <p>Hello Student</p>
+          <p>Here is Your Subject Wise Attendance Graph</p>
           <AttendanceGraph></AttendanceGraph>
         </div>
       </div>
